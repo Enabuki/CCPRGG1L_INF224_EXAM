15c3eb49b37f94bbc4e108d5f1a8f38bbbabc174
Andrei Jefferson S. Aseron <111546580+enabuki@users.noreply.github.com>
2022-09-06 09:41:19 +0800

    Update Student.java

--- exam_1/Student.java
+++ exam_1/Student.java
@@ -1,1 +1,44 @@
+public class Student {
+
+//Student attributes
+String surname;
+String firstname;
+char middleInitial;
+String dateOfBirth;
+int studentNumber;
+String studentEmailAddress;
+boolean iAmAwesome;
+
+
+
+//Student method
+
+public void sayMysurname() {
+    System.out.println("My surname is " + surname);
+}
+
+public void sayMyfirstname() {
+    System.out.println("My firstname is " + firstname);
+}
+
+public void sayMymiddleInitial() {
+    System.out.println("My middleInitial is " + middleInitial);
+}
+
+public void saydateOfBirth() {
+    System.out.println("My dateOfBirth is " + dateOfBirth);
+}
+
+public void sayMystudentNumber() {
+    System.out.println("My studentNumber is " + studentNumber);
+}
+
+public void sayMystudentEmailAddress() {
+    System.out.println("My studentEmailAddress is " + studentEmailAddress);
+}
+
+public void sayMyiAmAwesome() {
+    System.out.println("My iAmAwesome is " + iAmAwesome);
+}
+}
 
