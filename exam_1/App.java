785f1c620fe45e6901fadadbd5cc0cd6045fcece
Andrei Jefferson S. Aseron <111546580+enabuki@users.noreply.github.com>
2022-09-06 09:39:57 +0800

    Update App.java

--- exam_1/App.java
+++ exam_1/App.java
@@ -1,1 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+//Character Class
+Students Andrei = new Students();
 
+Andrei.surname = "Aseron";
+Andrei.firstname = "Andrei";
+Andrei.middleInitial = 'S';
+Andrei.dateOfBirth = "06/12/2004";
+Andrei.studentNumber = 2022104835;
+Andrei.studentEmailAddress = "aseronas@students.national-u.edu.ph";
+Andrei.iAmAwesome = true;
+Andrei.sayMystudentNumber();
+Andrei.sayMystudentEmailAddress();
+Andrei.sayMyiAmAwesome()
